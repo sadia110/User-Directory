20362458547e74fe0cbf0bba685818132ed45f56
Sadia Batool <sadia110@my.yorku.ca>
2020-07-31 02:10:27 -0400

    fixed components

--- my-app/src/components/EmployeeInfo.js
+++ my-app/src/components/EmployeeInfo.js
@@ -1,6 +1,29 @@
 import React from "react";
 
-  function EmployeeInfo(props) {
+  function EmployeeInfo(props) { 
+
+
+        let sortOrder = (props.order === "descending" ? 1 : -1)
+        let employeeList = props.employeeList
+        switch (props.sortBy) {
+            case ("fName"):
+                employeeList = employeeList.sort((a, b) => (a.name.first > b.name.first) ? sortOrder : -sortOrder);
+                break;
+            case ("lName"):
+                employeeList = employeeList.sort((a, b) => (a.name.last > b.name.last) ? sortOrder : -sortOrder);
+                break;
+            case ("city"):
+                employeeList = employeeList.sort((a, b) => (a.location.city > b.location.city) ? sortOrder : -sortOrder);
+                break;
+            case ("phoneNum"):
+                employeeList = employeeList.sort((a, b) => (a.phone > b.phone) ? sortOrder : -sortOrder);
+                break;
+            case ("email"):
+                employeeList = employeeList.sort((a, b) => (a.email > b.email) ? sortOrder : -sortOrder);
+                break;
+            default:
+                break;
+        }
      return (
         <div className="container">
         {props.employeeList.map(employee => (
@@ -8,7 +31,6 @@ import React from "react";
                 <div className="col-md-1"><img src={employee.picture.medium} alt="Emp Img" /></div>
                 <div className="col-md-1">{employee.name.first}</div>
                      <div className="col-md-1">{employee.name.last}</div>
-                     <div className="col-md-2">{employee.dob.date.split("T")[0]}</div>
                      <div className="col-md-2">{employee.location.city}</div>
                      <div className="col-md-2">{employee.phone}</div>
                      <div className="col-md-3">{employee.email}</div>
--- my-app/src/components/EmployeeList.js
+++ my-app/src/components/EmployeeList.js
@@ -1,79 +1,70 @@
 import React from "react";
- import axios from "axios";
+import axios from "axios";
+import EmployeeInfo from "./EmployeeInfo";
+import TableHeaders from "./TableHeaders";
 
-  function EmployeeInfo(props) {
-     return (
-         <div className="container">
-             {props.employeeList.map(employee => (
-                 <div className="row d-flex align-items-center">
-                     <div className="col-lg-1"><img src={employee.picture.medium} alt="Emp Img"/></div>
-                     <div className="col-lg-1">{employee.name.first}</div>
-                     <div className="col-lg-1">{employee.name.last}</div>
-                     <div className="col-lg-1">{employee.dob.date.split("T")[0]}</div>
-                     <div className="col-lg-2">{employee.location.city}</div>
-                     <div className="col-lg-2">{employee.location.state}</div>
-                     <div className="col-lg-3">{employee.email}</div>
-                     <div className="col-lg-1">{employee.phone}</div>
-                 </div>
-             ))}
-         </div>
-     )
- }
+class EmployeeList extends React.Component {
+  state = {
+    sortBy: "lName",
+    order: "descending",
+    search: this.props.query,
+    employees: []
+  };
 
-  class EmployeeList extends React.Component {
-     state = {
-         filter: this.props.filter,
-         order: this.props.order,
-         employees: []
-     };
-
-      async componentDidMount() {
-         try {
-             const employeeList = await axios.get("https://randomuser.me/api/?nat=CA&results=10");
-             this.setState({employees: employeeList.data.results})
-         } catch (err) {
-             console.log(err)
-         }
-     } 
-
-     sortList() {
-        let employeeList = this.state.employees
-        if (employeeList.length === 0) {
-            return employeeList;
-        } else {
-            employeeList.sort()
-        }
-        console.log(employeeList)
-        switch (this.state.filter) {
-            case "firstDescending":
-
-                 break;
-            case "firstAscending":
-
-                 break;
-            default:
-                return employeeList
-        }
+  async componentDidMount() {
+    try {
+      const employeeList = await axios.get("https://randomuser.me/api/?nat=CA&seed=fodder&results=40");
+      this.setState({ employees: employeeList.data.results })
+    } catch (err) {
+      console.log(err)
     }
+  }
 
-      render() {
-         console.log(this.state.employees)
-         return (
-             <div>
-                 <div className="container">
-                     <div className="row d-flex align-items-center" style={{ fontSize: "10pt", borderBottom: "2px solid black" }}>
-                         <div className="col-md-1"></div>
-                         <button className="btn btn-sm col-md-1">First Name</button>
-                         <button className="btn btn-sm col-md-1">Last Name</button>
-                         <button className="btn btn-sm col-md-2">City</button>
-                         <button className="btn btn-sm col-md-2">Phone</button>
-                         <button className="btn btn-sm col-md-3">Email</button>
-                     </div>
-                 </div>
-                 <EmployeeInfo employeeList={this.sortList()} />
-             </div>
-         )
-     }
- }
+  static getDerivedStateFromProps(nextProps) {
+    return {
+      search: nextProps.query,
+    };
+  }
 
-  export default EmployeeList
+  handleBtnClick = event => {
+    (this.state.sortBy !== event.target.name) || (this.state.order === "ascending") ? this.setState({ order: "descending" }) : this.setState({ order: "ascending" });
+
+    this.setState({ sortBy: event.target.name })
+  }
+
+  searchList() {
+    let employeeList = this.state.employees;
+    if (employeeList.length === 0) {
+      return employeeList;
+      // When the search state is updated the program will filter 
+    } else if (this.state.search !== "") {
+      employeeList = employeeList.filter(employee => {
+        return (
+          employee.name.first.toLowerCase().includes(this.state.search.toLowerCase()) ||
+          employee.name.last.toLowerCase().includes(this.state.search.toLowerCase()) ||
+          employee.location.city.toLowerCase().includes(this.state.search.toLowerCase()) ||
+          employee.phone.includes(this.state.search) ||
+          employee.email.toLowerCase().includes(this.state.search.toLowerCase())
+        );
+      })
+    }
+    return employeeList;
+  }
+
+  render() {
+    return (
+      <div className="container" style={{maxWidth:"1280px"}}>
+        <TableHeaders
+          handleBtnClick={this.handleBtnClick}
+          sortBy={this.state.sortBy}
+          order={this.state.order} />
+        <EmployeeInfo
+          employeeList={this.searchList()}
+          sortBy={this.state.sortBy}
+          order={this.state.order} />
+      </div>
+    )
+  }
+}
+
+export default EmployeeList;
