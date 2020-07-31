0cc171182a6f38c446b5f265cef24aba270ab636
Sadia Batool <sadia110@my.yorku.ca>
2020-07-31 00:58:40 -0400

    added table components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css'; 
-import EmployeeList from './components/EmployeeList'
+import './App.css';
+import EmployeeList from './components/EmployeeList' 
+
+
+function SortEmployees() {
+  return (
+    <div className="container">
+      <div className="row d-flex align-items-center" style={{fontSize: "10pt", borderBottom: "2px solid black"}}>
+        <div className="col-md-1"></div>
+        <div className="col-md-1">First Name</div>
+        <div className="col-md-1">Last Name</div>
+        <div className="col-md-2">City</div>
+        <div className="col-md-2">Phone</div>
+        <div className="col-md-3">Email</div>
+      </div>
+    </div>
+  )
+}
+
 
 
 function App() {
   return (
+
+
+
     <div> 
   
    <EmployeeList />
@@ -14,5 +34,6 @@ function App() {
     </div>
   );
 }
+
 
 export default App;
